b79467fa74c44355d79d192dfa52f7c0f835962e
Klismann Smoger Mottin <ksm09@inf.ufpr.br>
2011-06-06 22:26:18 -0300

    Adicionado header com algumas definições

---
+++ src/main.h
@@ -0,0 +1,21 @@
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef _PTHREAD_H
+#include <pthread.h>
+#endif
+
+
+#ifndef H_MAIN_H
+#define H_MAIN_H
+
+/* Constantes */
+#define TRUE 1
+#define FALSE 0
+#define TH_ATTR NULL
+#define MAX_THREADS 8
+
+typedef int nossaEstrutura; // A ser definido
+
+#endif
